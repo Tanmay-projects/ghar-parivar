0daa12e5d593554c1cf3797747c3726169ee2e08
Tanmay-projects <tanmayprojects2026@gmail.com>
2026-09-12 19:28:55 +0530

    Load Koriya family data on public site

--- slide2-loader.js
+++ slide2-loader.js
@@ -1,6 +1,11 @@
-/* Slide 2 image loader */
+/* Slide 2 image loader + public family data loader */
 (function(){
   const art=document.querySelector('.hero-art.art-two');
-  if(!art) return;
-  art.style.backgroundImage='url("slide2.jpg?v=20260911")';
+  if(art) art.style.backgroundImage='url("slide2.jpg?v=20260912")';
+  if(!document.querySelector('script[data-koriya-family]')){
+    const s=document.createElement('script');
+    s.src='koriya-family.js?v=20260912';
+    s.dataset.koriyaFamily='1';
+    document.body.appendChild(s);
+  }
 })();
