0929cef3d24cbe428751347a6050c506631bea6b
Tanmay-projects <tanmayprojects2026@gmail.com>
2026-09-14 11:38:13 +0530

    Add uploaded family photo to hero slide 3

--- slide2-loader.js
+++ slide2-loader.js
@@ -1,7 +1,11 @@
-/* Slide 2 image loader + public family data loader */
+/* Hero slide image loader + public family data loader */
 (function(){
-  const art=document.querySelector('.hero-art.art-two');
-  if(art) art.style.backgroundImage='url("slide2.jpg?v=20260912")';
+  const slide2=document.querySelector('.hero-art.art-two');
+  if(slide2) slide2.style.backgroundImage='url("slide2.jpg?v=20260912")';
+
+  const slide3=document.querySelector('.hero-art.art-three');
+  if(slide3) slide3.style.backgroundImage='url("IMG_8123.JPG?v=20260914")';
+
   if(!document.querySelector('script[data-koriya-family]')){
     const s=document.createElement('script');
     s.src='koriya-family.js?v=20260912';
